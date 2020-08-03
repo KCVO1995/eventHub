73f01588540cf50b04e429854419817e78194a69
kcvo <943514589@qq.com>
2020-08-03 14:30:31 +0800

    first commit

---
+++ src/index..ts
@@ -0,0 +1,16 @@
+class EventHub {
+  private cache: {[key: string]: Array<(data: unknown) => void>} = {}
+  on(eventName: string, cb: (data?: unknown) => void) {
+    this.cache[eventName] = this.cache[eventName] || []
+    this.cache[eventName].push(cb)
+  }
+  off(eventName: string, cb: (data?: unknown) => void) {
+    const index = (this.cache[eventName] || []).indexOf(cb)
+    if (index > 0) this.cache[eventName].splice(index, 1)
+  }
+  emit(eventName: string, message?: unknown) {
+    (this.cache[eventName] || []).forEach(fn => fn(message))
+  }
+}
+
+export default EventHub
